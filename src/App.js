14a3d7d2c96fb18da27ea237f03fec179c5ffac1
Eng Sze Kai <SourCream09@gmail.com>
2020-07-15 00:18:09 +0800

    fix: replaced < with an escaped char

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WIP <3
+          WIP&lt;3
         </p>
       </header>
     </div>
