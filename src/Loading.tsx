414ebc2d547222c22a07c4aeeb71c21b383bd4fe
Eng Sze Kai <SourCream09@gmail.com>
2020-07-15 17:24:25 +0800

    feat: added Kaiskii logo to loading screen

---
+++ src/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import KaiskiiLogo from './KaiskiiLogo.svg'
+import './Loading.css';
+
+const Loading = () => {
+  return(
+    <img src={KaiskiiLogo} className="Kaiskii-Logo"/>
+  );
+}
+
+export default Loading;
