86cb626b000fa24ba8ab716c99fe3c9f7437e460
Eng Sze Kai <SourCream09@gmail.com>
2020-07-16 12:17:05 +0800

    feat: implemented animation to loading blocks

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -6,7 +6,10 @@ const Loading = () => {
   const loadingBlocks = [];
   for(let y = 0; y < 6; y++) {
     for(let x = 0; x < 8; x++) {
-      loadingBlocks.push(<div className='LoadingSquare' style={{transform: `translate(${-400 + (x * 100)}px, ${-300 + (y * 100)}px)`}}/>)
+      let s = {'--r': `translate(${-400 + (x * 100)}px, ${-300 + (y * 100)}px)`} as React.CSSProperties;
+      loadingBlocks.push(
+        <div className='LoadingSquare' style={s}/>
+      );
     }
   }
 
