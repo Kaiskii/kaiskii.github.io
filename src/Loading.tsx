09bf692656c6a73da680c7e3c15ad3e4d122af42
Eng Sze Kai <SourCream09@gmail.com>
2020-07-16 11:47:07 +0800

    feat: implemented div grid covering logo for loading animation

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -3,9 +3,17 @@ import KaiskiiLogo from './KaiskiiLogo.svg'
 import './Loading.css';
 
 const Loading = () => {
+  const loadingBlocks = [];
+  for(let y = 0; y < 6; y++) {
+    for(let x = 0; x < 8; x++) {
+      loadingBlocks.push(<div className='LoadingSquare' style={{transform: `translate(${-400 + (x * 100)}px, ${-300 + (y * 100)}px)`}}/>)
+    }
+  }
+
   return(
     <div className='square'>
-      <img src={KaiskiiLogo} className='Kaiskii-Logo'/>
+      {loadingBlocks}
+      <img src={KaiskiiLogo} className='Kaiskii-Logo' alt='kaiskii'/>
     </div>
   );
 }
