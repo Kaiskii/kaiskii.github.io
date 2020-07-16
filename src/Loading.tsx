02157e6fc58311d3dacb837ae510d8c088f4b1d5
Eng Sze Kai <SourCream09@gmail.com>
2020-07-16 13:06:47 +0800

    feat: changed loading design

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -4,9 +4,14 @@ import './Loading.css';
 
 const Loading = () => {
   const loadingBlocks = [];
-  for(let y = 0; y < 6; y++) {
-    for(let x = 0; x < 8; x++) {
-      let s = {'--r': `translate(${-400 + (x * 100)}px, ${-300 + (y * 100)}px)`} as React.CSSProperties;
+  for(let y = 0; y < 9; y++) {
+    for(let x = 0; x < 13; x++) {
+      const ran = Math.random() * 9.0;
+      if (ran > 2.0) {
+        continue;
+      }
+      // animationDelay: `${((y * 10) + x)/10}s`
+      let s = {'--r': `translate(${-1000 + (x * 150)}px, ${-425 + (y * 100)}px)`, animationDelay: `${ran}s`} as React.CSSProperties;
       loadingBlocks.push(
         <div className='LoadingSquare' style={s}/>
       );
