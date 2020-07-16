3caf93f27912028004cbe37b4a2c4b2b26776c60
Eng Sze Kai <SourCream09@gmail.com>
2020-07-16 15:12:26 +0800

    feat: added random and delay to opacity

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -6,14 +6,18 @@ const Loading = () => {
   const loadingBlocks = [];
   for(let y = 0; y < 9; y++) {
     for(let x = 0; x < 12; x++) {
-      const ran = Math.random() * 9.0;
-      if (ran > 2.0) {
+      const ran = Math.random() * 18.0;
+      console.log(ran);
+      if (ran > 2) {
         continue;
       }
       // animationDelay: `${((y * 10) + x)/10}s`
       let s = {'--r': `translate(${-65 + (x * 10)}vw, ${-42.5 + (y * 10)}vh)`,  animationDelay: `${ran}s`} as React.CSSProperties;
+      let opc = {animationDelay: `${Math.random() * 10.0}s`, '--opc-ran': `${Math.random() * (4 - 3) + 3}s`} as React.CSSProperties;
       loadingBlocks.push(
-        <div className='LoadingSquare' style={s}/>
+        <div className='moreAnim' style={opc}>
+          <div className='LoadingSquare' style={s}/>
+        </div>
       );
     }
   }
