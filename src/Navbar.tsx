c09be542f63dd9b81da80ba6f8df3c55cebdb475
Eng Sze Kai <SourCream09@gmail.com>
2020-07-17 11:32:50 +0800

    fix: changed wordpress href

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           <img src={li} alt='linked-in'/>
         </a>
 
-        <a href='https://www.linkedin.com/in/kaiskii/' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
+        <a href='https://kappakaidev.wordpress.com/' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
           <img src={wp} alt='wordpress'/>
         </a>
       </div>
