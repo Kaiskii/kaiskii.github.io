c2993d76f2c92d0b6500546e4f2a763cd3f785b6
Eng Sze Kai <SourCream09@gmail.com>
2020-07-17 11:23:57 +0800

    feat: added social medias to navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import './Navbar.css'
+import li from './linkedin.png';
+import tw from './twitter.png';
+import './Navbar.css';
+
 
 const Navbar = () => {
   return (
-    <div className='Navbar'/>
+    <div className='Navbar'>
+      <div className='Navbar-SM-Align'>
+        <a href='https://twitter.com/_Kaiskii' target='_blank' rel='noopener noreferrer' className='Navbar-Btn' style={{ marginTop: '0.6vh', }}>
+          <img src={tw} alt='twitter'/>
+        </a>
+
+        <a href='https://www.linkedin.com/in/kaiskii/' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
+          <img src={li} alt='linked-in'/>
+        </a>
+      </div>
+    </div>
   );
 }
 
