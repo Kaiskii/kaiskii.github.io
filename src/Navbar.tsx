a9b5fe3e376e868f32019486d9a2309f79ea2795
Eng Sze Kai <SourCream09@gmail.com>
2020-07-17 11:32:02 +0800

    feat: changed icon and added wordpress

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import li from './linkedin.png';
 import tw from './twitter.png';
+import wp from './wordpress.png';
 import './Navbar.css';
 
 
@@ -8,12 +9,16 @@ const Navbar = () => {
   return (
     <div className='Navbar'>
       <div className='Navbar-SM-Align'>
-        <a href='https://twitter.com/_Kaiskii' target='_blank' rel='noopener noreferrer' className='Navbar-Btn' style={{ marginTop: '0.6vh', }}>
+        <a href='https://twitter.com/_Kaiskii' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
           <img src={tw} alt='twitter'/>
         </a>
 
         <a href='https://www.linkedin.com/in/kaiskii/' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
           <img src={li} alt='linked-in'/>
+        </a>
+
+        <a href='https://www.linkedin.com/in/kaiskii/' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
+          <img src={wp} alt='wordpress'/>
         </a>
       </div>
     </div>
