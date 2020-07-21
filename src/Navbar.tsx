4515c6493e54d87fa3fbe619cda704c00750a78e
Eng Sze Kai <SourCream09@gmail.com>
2020-07-21 15:04:44 +0800

    feat: added drop shadow and scaling on hover

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -9,15 +9,15 @@ const Navbar = () => {
   return (
     <div className='Navbar'>
         <a href='https://twitter.com/_Kaiskii' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
-          <img src={tw} draggable='false' onMouseDown={(event) => {if (event.preventDefault) event.preventDefault()}} alt='twitter'/>
+          <img src={tw} className='SM-Btn' draggable='false' onMouseDown={(event) => {if (event.preventDefault) event.preventDefault()}} alt='twitter'/>
         </a>
 
         <a href='https://www.linkedin.com/in/kaiskii/' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
-          <img src={li} draggable='false' onMouseDown={(event) => {if (event.preventDefault) event.preventDefault()}} alt='linked-in'/>
+          <img src={li} className='SM-Btn' draggable='false' onMouseDown={(event) => {if (event.preventDefault) event.preventDefault()}} alt='linked-in'/>
         </a>
 
         <a href='https://kappakaidev.wordpress.com/' target='_blank' rel='noopener noreferrer' className='Navbar-Btn'>
-          <img src={wp} draggable='false' onMouseDown={(event) => {if (event.preventDefault) event.preventDefault()}} alt='wordpress'/>
+          <img src={wp} className='SM-Btn' draggable='false' onMouseDown={(event) => {if (event.preventDefault) event.preventDefault()}} alt='wordpress'/>
         </a>
     </div>
   );
